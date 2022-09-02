b5a50e51495b612949431ed72c5a4d35988efcee
gabriel soares <110830279+Gabriel3410@users.noreply.github.com>
2022-09-01 21:53:21 -0300

    Update alter_table.sql

--- workbench/colegio_santoinacio/alter_table.sql
+++ workbench/colegio_santoinacio/alter_table.sql
@@ -12,3 +12,15 @@ ADD observacao VARCHAR(80);
 select * from produtos;
 
 SHOW COLUMNS FROM produtos;
+
+ALTER TABLE produtos
+MODIFY COLUMN observacao VARCHAR(255);
+
+ALTER TABLE produtos
+MODIFY COLUMN observacao TEXT;
+
+ALTER TABLE produtos
+DROP COLUMN descricao;
+
+ALTER TABLE produtos
+MODIFY COLUMN observacao VARCHAR(255);
