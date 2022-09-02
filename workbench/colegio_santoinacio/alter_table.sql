6448ec1b2c8d07cda752b25c44af049503c708af
gabriel soares <110830279+Gabriel3410@users.noreply.github.com>
2022-09-01 21:32:34 -0300

    Update alter_table.sql

--- workbench/colegio_santoinacio/alter_table.sql
+++ workbench/colegio_santoinacio/alter_table.sql
@@ -1,3 +1,14 @@
 use bd2;
 
+ALTER TABLE produtos
+ADD descricao TEXT;
+
+ALTER TABLE produtos
+ADD valor FLOAT;
+
+ALTER TABLE produtos
+ADD observacao VARCHAR(80);
+
 select * from produtos;
+
+SHOW COLUMNS FROM produtos;
